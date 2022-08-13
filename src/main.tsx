10ec30d8d75ffe23b3caa65c87fc26a2f596f690
Aaron Burdick <burdick.aaron@gmail.com>
2022-08-13 18:03:13 +0900

    feat: give user something to look at while app loads

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,11 @@
 import {render} from 'solid-js/web'
 import App from './App'
 
+// Render Solid App
 render(() => <App />, document.getElementById('root') as HTMLElement);
+
+// Remove placeholder splash for App
+const splashEl = document.getElementById("app-splash");
+if (splashEl) {
+  splashEl.remove();
+}

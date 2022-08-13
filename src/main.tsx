9e53971a6c898252949364c0789388cbea0cda1d
Aaron Burdick <burdick.aaron@gmail.com>
2022-08-13 19:14:12 +0900

    chore: comments

--- src/main.tsx
+++ src/main.tsx
@@ -4,30 +4,37 @@ import log from "loglevel";
 import AppContextProvider from "./AppContextProvider"
 import App from './App'
 
-log.setLevel(0)
-
-log.info(import.meta.env.VITE_SOME_KEY);
-
+// ** RUNTIME ENVIRONMENT SETUP **
+//
 // Reference for levels of logs in 'loglevel' pkg
 // log.error("error");
 // log.warn("warn");
 // log.info("info");
 // log.debug("debug");
 // log.trace("trace");
+//
+if (import.meta.env.PROD) {
+  log.setLevel(4)
+}
+if (import.meta.env.DEV) {
+  log.setLevel(0)
+}
 
+// ** APP ** 
+render(
+  () => (
+    <AppContextProvider
+      fbApp={null}
+      fbAuth={null}
+      fbUser={null}
+    >
+      <App />
+    </AppContextProvider>
+  ),
+  document.getElementById('root') as HTMLElement
+);
 
-// Render Solid App
-render(() =>
-  <AppContextProvider
-    fbApp={null}
-    fbAuth={null}
-    fbUser={null}
-  >
-    <App />
-  </AppContextProvider>
-  , document.getElementById('root') as HTMLElement);
-
-// Remove placeholder splash for App
+// ** CLEAN UP FROM BEFORE APP IS LOADED ** 
 const splashEl = document.getElementById("app-splash");
 if (splashEl) {
   splashEl.remove();
